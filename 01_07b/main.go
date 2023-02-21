0d548fa4fecbf9eddda7a235b1fe73dfd4daeac0
Felipe Puentes <feliipepuentes@gmail.com>
2023-02-21 21:21:12 +0000

    adding solution to challenge 7

--- 01_07b/main.go
+++ 01_07b/main.go
@@ -8,11 +8,46 @@ import (
 // isBalanced returns whether the given expression
 // has balanced brackets.
 func isBalanced(expr string) bool {
-	panic("NOT IMPLEMENTED")
+	var closingComparator string
+
+	for _, r := range expr {
+		if o, c := isOpeningSymbol(r); o {
+			closingComparator = string(c) + closingComparator
+			continue
+		}
+
+		if r != rune(closingComparator[0]) {
+			return false
+		}
+
+		if len(closingComparator) > 0 {
+			closingComparator = closingComparator[1:]
+		}
+
+	}
+
+	if len(closingComparator) > 0 {
+		return false
+	}
+
+	return true
+}
+
+func isOpeningSymbol(symbol rune) (bool, rune) {
+	switch symbol {
+	case '(':
+		return true, ')'
+	case '[':
+		return true, ']'
+	case '{':
+		return true, '}'
+	default:
+		return false, ' '
+	}
 }
 
 // printResult prints whether the expression is balanced.
-func printResult(expr string, balanced bool){ 
+func printResult(expr string, balanced bool) {
 	if balanced {
 		log.Printf("%s is balanced.\n", expr)
 		return
