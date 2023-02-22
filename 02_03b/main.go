b188e2b803ebab79ab51606852076a33761ead4d
Felipe Puentes <feliipepuentes@gmail.com>
2023-02-22 17:16:49 +0000

    solving challenge 02_03

--- 02_03b/main.go
+++ 02_03b/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 // the number of attendees we need to serve lunch to
-const consumerCount = 300
+const (
+	consumerCount = 300
+)
 
 // foodCourses represents the types of resources to pass to the consumers
 var foodCourses = []string{
@@ -14,17 +19,52 @@ var foodCourses = []string{
 
 // takeLunch is the consumer function for the lunch simulation
 // Change the signature of this function as required
-func takeLunch(name string) {
-	panic("NOT IMPLEMENTED YET")
+func takeLunch(consumerName string, food []chan string, doneSignal chan<- struct{}) {
+	for _, ch := range food {
+		fmt.Printf("%s takes %s\n", consumerName, <-ch)
+	}
+
+	doneSignal <- struct{}{}
 }
 
 // serveLunch is the producer function for the lunch simulation.
 // Change the signature of this function as required
-func serveLunch(course string) {
-	panic("NOT IMPLEMENTED YET")
+func serveLunch(food string, out chan<- string, done <-chan struct{}) {
+	for {
+		select {
+		case out <- food:
+		case <-done:
+			return
+		}
+	}
+}
+
+func startServingFood(numberOfCosumers int) {
+	courses := make([]chan string, len(foodCourses))
+	doneServing := make(chan struct{})
+	doneEating := make(chan struct{})
+
+	for index, value := range foodCourses {
+		ch := make(chan string)
+		courses[index] = ch
+		go serveLunch(value, ch, doneServing)
+	}
+
+	for i := 0; i < numberOfCosumers; i++ {
+		name := fmt.Sprintf("Consumer %d", i)
+		go takeLunch(name, courses, doneEating)
+	}
+
+	for i := 0; i < numberOfCosumers; i++ {
+		<-doneEating
+	}
+
+	close(doneServing)
 }
 
 func main() {
 	log.Printf("Welcome to the conference lunch! Serving %d attendees.\n",
 		consumerCount)
+
+	startServingFood(consumerCount)
 }
